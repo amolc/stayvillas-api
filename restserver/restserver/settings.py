43805863f8469cdbe52a08a17105ccba1cfc7997
Amol C <amolch001@gmail.com>
2024-09-12 12:25:09 +0530

    added the postgres database

--- restserver/restserver/settings.py
+++ restserver/restserver/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,11 +95,25 @@ SILENCED_SYSTEM_CHECKS = ["auth.E003"]
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ["MYSQL_DB"],
+        'USER': os.environ["MYSQL_USER"],
+        'PASSWORD': os.environ['MYSQL_PASSWORD'],
+        'HOST': os.environ["MYSQL_HOST"], 
+       # 'HOST': os.environ["MYSQL_HOST"], # Or an IP Address that your DB is hosted on
+        'PORT': os.environ.get('MYSQL_PORT', '3306'),
+        'default-character-set':'utf8'
     }
+
 }
 
 
