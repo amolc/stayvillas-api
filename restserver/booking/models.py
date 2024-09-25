8434cc2bb8a1a1d5d4ef14a6ccb04411395d9859
KrushnaLavhare1 <krushnalavhare@outlook.com>
2024-09-25 12:18:54 +0530

    booking model change

--- restserver/booking/models.py
+++ restserver/booking/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class Booking(models.Model):
     org_id = models.PositiveIntegerField(null=True, blank=True)
 
@@ -34,6 +31,7 @@ class Booking(models.Model):
     booking_status = models.CharField(max_length=20, choices=BOOKING_STATUS_CHOICES, default=PENDING)
 
     num_guests = models.IntegerField(default=1)
+    num_rooms = models.IntegerField(default=1)  # Add number of rooms
     special_requests = models.TextField(blank=True, null=True)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
 
