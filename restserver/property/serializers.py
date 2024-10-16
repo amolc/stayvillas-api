219b606a7531048e141891eb6adbaf39a2565cf6
KrushnaLavhare1 <krushnalavhare@outlook.com>
2024-10-16 15:17:46 +0530

    added amenities filed

--- restserver/property/serializers.py
+++ restserver/property/serializers.py
@@ -15,7 +15,7 @@ class PropertySerializer(serializers.ModelSerializer):
             'bedroom1_image', 'description1', 'bedroom2_image', 'description2',
             'bedroom3_image', 'description3', 'bedroom4_image', 'description4',
             'total_bedroom_size', 'square_feet', 'location_url', 'great_for', 
-            'other_images','img', 'address1', 'address2', 'pincode', 
+            'other_images','img', 'address1', 'address2', 'pincode',  'amenities',
             'created_date', 'created_by', 'updated_date', 'updated_by'
         ]
         read_only_fields = ['id', 'created_date', 'created_by', 'updated_date', 'updated_by']
