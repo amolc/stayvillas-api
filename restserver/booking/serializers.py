18020462b9132d3d0ce4e4bd2cc68149391c857c
KrushnaLavhare1 <krushnalavhare@outlook.com>
2024-10-18 15:53:39 +0530

    added fields booking

--- restserver/booking/serializers.py
+++ restserver/booking/serializers.py
@@ -1,10 +1,29 @@
 from rest_framework import serializers
-from .models import Booking, EventBooking
+from .models import Booking, EventBooking, Customer
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['name', 'email', 'mobile_no']
 
 class BookingSerializer(serializers.ModelSerializer):
+    customers = CustomerSerializer(many=True)
+
     class Meta:
         model = Booking
         fields = '__all__'
+
+    def create(self, validated_data):
+        # Extract customer data from validated_data
+        customers_data = validated_data.pop('customers')
+        # Create the Booking instance
+        booking = Booking.objects.create(**validated_data)
+        # Create Customer instances
+        for customer_data in customers_data:
+            customer, created = Customer.objects.get_or_create(**customer_data)
+            booking.customers.add(customer)  # Associate customer with booking
+        return booking
+
 class EventBookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = EventBooking
