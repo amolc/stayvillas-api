b2d4b90ff801a2228d326995f9d2c309eb5492df
KrushnaLavhare1 <krushnalavhare@outlook.com>
2024-11-11 17:00:27 +0530

    modify fields in destination

--- restserver/destination/models.py
+++ restserver/destination/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Destination(models.Model):
     country = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
-    img = models.URLField(max_length=255, null=True, blank=True)
+    img = models.TextField(null=True, blank=True)
 
     class Meta:
         db_table = 'Destination'
