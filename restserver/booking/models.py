89debf5f55a4a7480005548c96948d749f63eece
KrushnaLavhare1 <krushnalavhare@outlook.com>
2024-11-25 10:18:34 +0530

    fixed some chnages in booking

--- restserver/booking/models.py
+++ restserver/booking/models.py
@@ -10,32 +10,8 @@ class Customer(models.Model):
 class Booking(models.Model):
     org_id = models.PositiveIntegerField(null=True, blank=True)
 
-    # # Property type choices
-    # VILLA = 'Villa'
-    # BUNGALOW = 'Bungalow'
-    # HOTEL = 'Hotel'
-    # PROPERTY_TYPE_CHOICES = [
-    #     (VILLA, 'Villa'),
-    #     (BUNGALOW, 'Bungalow'),
-    #     (HOTEL, 'Hotel')
-    # ]
-    # property_type = models.CharField(max_length=50, choices=PROPERTY_TYPE_CHOICES)
-
-    # guest_name = models.CharField(max_length=255)
-    # email = models.EmailField()
-    # mobile_no = models.CharField(max_length=15)
     check_in_date = models.DateField()
     check_out_date = models.DateField()
-
-    # PENDING = 'Pending'
-    # CONFIRMED = 'Confirmed'
-    # CANCELLED = 'Cancelled'
-    # BOOKING_STATUS_CHOICES = [
-    #     (PENDING, 'Pending'),
-    #     (CONFIRMED, 'Confirmed'),
-    #     (CANCELLED, 'Cancelled')
-    # ]
-    # booking_status = models.CharField(max_length=20, choices=BOOKING_STATUS_CHOICES)
 
     num_guests = models.IntegerField(default=1)
     num_rooms = models.IntegerField(default=1)  # Add number of rooms
