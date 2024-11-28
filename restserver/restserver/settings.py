a86b39e1af9da50a4f162c40652696a2d9d64298
KrushnaLavhare1 <krushnalavhare@outlook.com>
2024-11-28 20:19:44 +0530

    added configs without key in settings.py

--- restserver/restserver/settings.py
+++ restserver/restserver/settings.py
@@ -19,6 +19,9 @@ from pathlib import Path
 # Take environment variables from .env file
 # environ.Env.read_env()
 
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", "")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", "")
+AWS_REGION_NAME = os.getenv("AWS_REGION_NAME", "ap-southeast-1")
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
