77839a5c5b33bcdfedcd5e35b1cf82bc3e4bf1c5
Lusayo Nyondo <lusayohendy@gmail.com>
2024-12-27 16:34:40 +0200

    Added dot env reading for AWS keys.

--- restserver/enquiry/utils.py
+++ restserver/enquiry/utils.py
@@ -1,14 +1,16 @@
 import os
-import boto3
-from botocore.exceptions import NoCredentialsError, PartialCredentialsError
+from dotenv import load_dotenv
+import boto3  # type: ignore
+from botocore.exceptions import (  # type: ignore
+    NoCredentialsError,
+    PartialCredentialsError
+)
 
+load_dotenv()
 
 AWS_REGION = os.getenv('AWS_REGION_NAME', 'ap-southeast-1')
-AWS_KEY_ID = os.getenv('AWS_SECRET_KEY_ID', 'AKIAXEVXYOKKUVONZJEZ')
-AWS_SECRET_ACCESS_KEY = os.getenv(
-    'AWS_SECRET_ACCESS_KEY',
-    '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'
-)
+AWS_KEY_ID = os.getenv('AWS_SECRET_KEY_ID', None)
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', None)
 
 CHARSET = "UTF-8"
 SENDER = "support@stayvillas.co"  # Replace with your verified SES email
@@ -16,6 +18,9 @@ SENDER = "support@stayvillas.co"  # Replace with your verified SES email
 
 def send_booking_enquiry_mail(recipient, subject, body_text, body_html):
     try:
+        if AWS_KEY_ID is None or AWS_SECRET_ACCESS_KEY is None:
+            raise NoCredentialsError()
+
         ses_client = boto3.client(
             'ses',
             aws_access_key_id=AWS_KEY_ID,
